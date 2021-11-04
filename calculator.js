487ff00d77b8151d3c414c1a1efcf38d0538ba54
jen <63580680+limjenli@users.noreply.github.com>
2021-11-04 18:10:50 +0800

    Finishing up

--- calculator.js
+++ calculator.js
@@ -11,8 +11,8 @@ function updateTheme() {
 		themethree.classList.remove("active")
 		themeone.classList.add("active")
 		document.body.classList.remove("theme-3")
-		// document.body.classList.toggle("theme-1")
 	} else if (theme == 2) {
+		document.body.classList.remove("preload")
 		themeone.classList.remove("active")
 		themetwo.classList.add("active")
 		document.body.classList.add("theme-2")
@@ -27,7 +27,6 @@ function updateTheme() {
 themeToggle.forEach((div) => {
 	div.addEventListener("click", () => {
 		theme != 3 ? theme++ : (theme = 1)
-		console.log(theme)
 		updateTheme()
 	})
 })
@@ -36,11 +35,7 @@ themeToggle.forEach((div) => {
 
 let outputDisplay = document.querySelector(".number-output")
 let keys = document.querySelectorAll("button")
-let output
-
-function updateDisplay(output) {
-	outputDisplay.textContent = output ? commaSeparator(output) : ""
-}
+let output = 0
 
 function commaSeparator(num) {
 	let str = num.toString().split(".")
@@ -48,38 +43,49 @@ function commaSeparator(num) {
 	return str.join(".")
 }
 
-updateDisplay(233223)
+function updateDisplay(output) {
+	outputDisplay.textContent = commaSeparator(output)
+}
 
-// keys.forEach((key) => {
-// 	key.addEventListener("click", (event) => {
-// 		if (event.target.dataset.action === undefined) {
-// 			// console.log(key.innerHTML)
-// 			console.log(event.target.dataset.action)
-// 			output = key.innerHTML
-// 		}
-// 	})
+updateDisplay(output)
 
-// 	console.log(output)
-// 	updateDisplay(output)
-// })
-
-// keys.addEventListener("click", (e) => {
-// 	if (e.target.matches("button")) return
-// 	const key = e.target
-// 	console.log(key)
-// })
-
-// check if number is pressed
+// for each key press
+// determine if it's a number
 // if yes, add to display
-// check if number is pressed
-// if yes, add to display
+// if no, perform operation (delete, plus, minus, divide, multiply, add decimal)
 
-// if no, perform function
-// if del, del last number in output
-// if plus, sum previous num and next num
-// if minus, minus numbers
-// if multiply, times numbers
-// if dot, add a decimal
-// if divide, divide
-// if reset, reset
-// if equals, update output display with output
+keys.forEach((key) => {
+	key.addEventListener("click", (e) => {
+		// if key is a number
+		if (
+			e.target.matches("button") &&
+			e.target.dataset.action == undefined
+		) {
+			output = output == 0 ? key.textContent : output + key.textContent
+		} else if (e.target.dataset.action == "decimal") {
+			output += "."
+		} else if (e.target.dataset.action == "delete") {
+			output.length == 1
+				? (output = 0)
+				: (output = output ? output.slice(0, -1) : output)
+		} else if (e.target.dataset.action == "reset") {
+			output = 0
+		} else if (e.target.dataset.action == "plus") {
+			output = output + "+"
+		} else if (e.target.dataset.action == "minus") {
+			output = output + "-"
+		} else if (e.target.dataset.action == "multiply") {
+			output = output + "*"
+		} else if (e.target.dataset.action == "divide") {
+			output = output + "/"
+		} else if (e.target.dataset.action == "equals") {
+			output = eval(output)
+				.toFixed(5)
+				.replace(/0+$/, "") // removes trailing zeros
+				.replace(/\.$/, "") // removes trailing decimal
+			updateDisplay(output)
+		}
+
+		updateDisplay(output)
+	})
+})
